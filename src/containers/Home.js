e33572e4aab5a7319c5a251560010e5cbfea939e
Carl-Dev <c_mungazi@hotmail.co.uk>
2019-09-19 22:23:25 +0100

    Delete old code

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,29 +1,10 @@
 import React from 'react'
 import { RouteData } from 'react-static'
-import { Link } from '@reach/router'
-import CategoryCard from '../components/Cards/CategoryCard'
 
 export default () => {
   return (
     <RouteData>
-      {
-        ({ categories }) => {
-          return (
-            <div className="flex flex-row flex-wrap justify-center">
-              {
-                categories.map(category => {
-                  return ( 
-                    <Link className="" to={`/${category.name}`} key={category.name}>
-                      <CategoryCard                         
-                        category={category} 
-                      />
-                    </Link>
-                  )
-                })
-              }
-            </div>
-        )}
-      }
+      <div>Hello</div>
     </RouteData>
   )
 }
